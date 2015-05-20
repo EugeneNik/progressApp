236f9462e42164b96ef4996a18df4befa9283678
EugeneNik <evginik1990@gmail.com>
2015-05-20 14:26:33 +0400

    commit
    Fixed #9
    Fixed #5

--- src/ui/StatusBar.java
+++ src/ui/StatusBar.java
@@ -4,6 +4,7 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
@@ -18,14 +19,15 @@ public class StatusBar extends BorderPane {
 
     private StatusBar self;
     private Text text;
-    private Text progressText;
+    private Label progressText;
     private ProgressBar progressBar;
 
     public StatusBar() {
         self = this;
         text = new Text();
         progressBar = new ProgressBar();
-        progressText = new Text();
+        progressText = new Label();
+        progressText.getStyleClass().addAll("progress-text");
 
         StackPane pane = new StackPane();
         pane.getChildren().addAll(progressBar, progressText);
@@ -37,6 +39,11 @@ public class StatusBar extends BorderPane {
         progressBar.progressProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                if (newValue != null && newValue.doubleValue() == 1.0) {
+                    progressBar.getStyleClass().addAll("green-bar");
+                } else if (progressBar.getStyleClass().contains("green-bar")) {
+                    progressBar.getStyleClass().remove("green-bar");
+                }
                 progressBar.setVisible(newValue != null && newValue.longValue() >= 0);
                 DecimalFormat format = new DecimalFormat("#0.00");
                 System.out.println(progressBar.getProgress() * 100.0);
