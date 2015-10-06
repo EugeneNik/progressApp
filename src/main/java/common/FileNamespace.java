f8ac2af7f75dfa844f08bf5cc25816a1c99520d0
Nikiforov <nikiforov@magenta-technology.ru>
2015-10-06 17:32:22 +0400

    remove resource to maven folders

--- src/main/java/common/FileNamespace.java
+++ src/main/java/common/FileNamespace.java
@@ -16,32 +16,32 @@ public interface FileNamespace {
     String USER_PROFILE = "backup/user_profile.xml";
 
     String CSS = "/skin.css";
-    String SETTINGS = "resources/settings.properties";
-    String REFRESH = "resources/icons/refresh.png";
-    String STOP = "resources/icons/stop.png";
-    String DEFAULT_ACHIEVEMENT_IMAGE = "resources/icons/default_achievement.png";
-    String GLOBAL_SEARCH = "resources/icons/global.png";
+    String SETTINGS = "/settings.properties";
+    String REFRESH = "/icons/refresh.png";
+    String STOP = "/icons/stop.png";
+    String DEFAULT_ACHIEVEMENT_IMAGE = "/icons/default_achievement.png";
+    String GLOBAL_SEARCH = "/icons/global.png";
 
     //achievements
 
-    String COMPLETED_TASKS_ALL = "resources/icons/completedTasksAll.png";
-    String COMPLETED_TASKS_1 = "resources/icons/completedTasks1.png";
-    String COMPLETED_TASKS_10 = "resources/icons/completedTasks10.png";
-    String COMPLETED_TASKS_20 = "resources/icons/completedTasks20.png";
-    String COMPLETED_TASKS_50 = "resources/icons/completedTasks50.png";
-    String COMPLETED_TASKS_100 = "resources/icons/completedTasks100.png";
-    String COMPLETED_TASKS_1000 = "resources/icons/completedTasks1000.png";
+    String COMPLETED_TASKS_ALL = "/icons/completedTasksAll.png";
+    String COMPLETED_TASKS_1 = "/icons/completedTasks1.png";
+    String COMPLETED_TASKS_10 = "/icons/completedTasks10.png";
+    String COMPLETED_TASKS_20 = "/icons/completedTasks20.png";
+    String COMPLETED_TASKS_50 = "/icons/completedTasks50.png";
+    String COMPLETED_TASKS_100 = "/icons/completedTasks100.png";
+    String COMPLETED_TASKS_1000 = "/icons/completedTasks1000.png";
 
-    String COMPLETE_5_IN_1_DAY = "resources/icons/complete5in1Day.png";
-    String COMPLETE_10_IN_1_DAY = "resources/icons/complete10in1Day.png";
-    String COMPLETE_20_IN_1_DAY = "resources/icons/complete20in1Day.png";
+    String COMPLETE_5_IN_1_DAY = "/icons/complete5in1Day.png";
+    String COMPLETE_10_IN_1_DAY = "/icons/complete10in1Day.png";
+    String COMPLETE_20_IN_1_DAY = "/icons/complete20in1Day.png";
 
-    String MINUTES_ONLINE_10 = "resources/icons/minutesOnline10.png";
-    String MINUTES_ONLINE_1440 = "resources/icons/minutesOnline1440.png";
+    String MINUTES_ONLINE_10 = "/icons/minutesOnline10.png";
+    String MINUTES_ONLINE_1440 = "/icons/minutesOnline1440.png";
 
-    String DAYS_FROM_START_365 = "resources/icons/daysStart365.png";
-    String DAYS_IN_ROW_7 = "resources/icons/daysInRow7.png";
+    String DAYS_FROM_START_365 = "/icons/daysStart365.png";
+    String DAYS_IN_ROW_7 = "/icons/daysInRow7.png";
 
-    String COMPLETED_STORY_POINTS_500 = "resources/icons/completedStoryPoints500.png";
-    String COMPLETED_STORY_POINTS_1000 = "resources/icons/completedStoryPoints1000.png";
+    String COMPLETED_STORY_POINTS_500 = "/icons/completedStoryPoints500.png";
+    String COMPLETED_STORY_POINTS_1000 = "/icons/completedStoryPoints1000.png";
 }
